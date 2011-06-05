41dcbf7ce64d9680b8e506b8f456073c1ef22e8d
arvida <arvid@winstondesign.se>
2011-06-05 21:31:40 +0200

    Added exec hash bang

--- example/simple.rb
+++ example/simple.rb
@@ -1,3 +1,4 @@
+#!/usr/bin/env ruby
 require 'rubygems'
 require 'runkeeper_activities'
 
@@ -9,5 +10,4 @@ runkeeper_user = RunKeeperActivities::User.find_by_username(username)
 runkeeper_user.activities[0...5].each do |a|
   puts "* On #{a.start_time.strftime("%A %d %B")} #{a.summery}"
   puts "  Duration: #{a.duration} #{a.user.duration_unit} - Avg. speed: #{a.speed} #{a.user.speed_unit}"
-  puts ""
 end
